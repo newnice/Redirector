9add8f0749ff0cf02af73c9759c3cd6a3ae9a2ef
margo <margobts@gmail.com>
2024-05-23 15:33:09 +0300

    initial commit

---
+++ src/main/java/ru/loony/RedirectorApplication.java
@@ -0,0 +1,13 @@
+package ru.loony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedirectorApplication.class, args);
+    }
+
+}
